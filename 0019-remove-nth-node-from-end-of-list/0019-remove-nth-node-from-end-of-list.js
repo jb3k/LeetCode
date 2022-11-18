bd245847511d8c6a2771294f14f32bed48b9451d
JB Kam <92565826+jb3k@users.noreply.github.com>
2022-11-18 15:47:54 -0800

    Time: 76 ms (84.99%), Space: 42.8 MB (57.42%) - LeetHub

---
+++ 0019-remove-nth-node-from-end-of-list/0019-remove-nth-node-from-end-of-list.js
@@ -0,0 +1,41 @@
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * @param {ListNode} head
+ * @param {number} n
+ * @return {ListNode}
+ */
+ var removeNthFromEnd = function(head, n) {
+    const holder = new ListNode();
+
+    holder.next = head;
+
+    const fast = moveFast(holder, n);   /* Time O(N) */
+    const slow = moveSlow(holder, fast);/* Time O(N) */
+
+    slow.next = slow.next.next || null;
+
+    return holder.next;
+};
+
+const moveFast = (fast, n) => {
+    for (let i = 1; i <= (n + 1); i++) {/* Time O(N) */
+        fast = fast.next;
+    }
+
+    return fast;
+}
+
+const moveSlow = (slow, fast) => {
+    while (fast) {                     /* Time O(N) */
+        slow = slow.next;
+        fast = fast.next;
+    }
+
+    return slow;
+}
