2daa4f632976b694da4db9fd89e20609f420d56b
JB Kam <92565826+jb3k@users.noreply.github.com>
2022-11-28 13:39:13 -0800

    Time: 124 ms (63.91%), Space: 42.4 MB (95.27%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.js
+++ 0242-valid-anagram/0242-valid-anagram.js
@@ -4,24 +4,21 @@
  * @return {boolean}
  */
 function isAnagram(a, b){
-  let arr = []
-  let obj1 = {};
+  let obj = {};
   
   if(a.length !== b.length) return false;
   
-  for(let i = 0; i < a.length; i++){
-  	let word1 = a[i]
-    
-	if(arr.includes(word1)){
-    	obj1[word1] += 1
-    } else {
-    	arr.push(word1)
-      	obj1[word1] = 1
+    for(let i = 0; i < a.length; i++){
+    	let char = a[i]
+        if(obj[char]){
+        	obj[char]++
+        } else {
+        	obj[char] = 1
+        }
     }
-  }
 
   for (let letter of b){
-  	if (obj1[letter]) obj1[letter] -=1
+  	if (obj[letter]) obj[letter] -=1
     else return false
   
   }
