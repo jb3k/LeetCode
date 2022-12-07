d6cc6b51d32d7be1015ff930c7be5ac551a19a02
JB Kam <92565826+jb3k@users.noreply.github.com>
2022-12-07 11:43:34 -0800

    Time: 79 ms (76.71%), Space: 41.9 MB (83.85%) - LeetHub

---
+++ 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.js
@@ -0,0 +1,25 @@
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number}
+ */
+var search = function(nums, target) {
+
+    let [l, r] = [0, nums.length -1]
+
+    while(l <= r){
+        let mid = Math.floor((l + r)/2) 
+        if(nums[mid] === target) return mid
+
+        if(nums[l] <= nums[mid]){
+            if(target > nums[mid] || target < nums[l]) l = mid + 1
+            else r = mid - 1
+        }else {
+            if(target < nums[mid] || target > nums[r]) r = mid - 1
+            else l = mid + 1
+
+        }
+    }
+    return -1
+
+};
