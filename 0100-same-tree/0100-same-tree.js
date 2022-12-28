cfb85dad5e9beb4d5e27ad92a43e52d8c7612e4b
JB Kam <92565826+jb3k@users.noreply.github.com>
2022-12-27 23:37:34 -0800

    Time: 59 ms (95.32%), Space: 41.8 MB (94.49%) - LeetHub

--- 0100-same-tree/0100-same-tree.js
+++ 0100-same-tree/0100-same-tree.js
@@ -13,12 +13,7 @@
  */
 var isSameTree = function(root, subRoot) {
     
-//     if(!p && !q) return true
-//     if(!p || !q || p.val !== q.val) return false
-    
-//     if(!isSameTree(p.left, q.left) || !isSameTree(p.right, q.right)) return false
-//     return true
-        const hasReachedEnd = !(root && subRoot)
+    const hasReachedEnd = !(root && subRoot)
     if (hasReachedEnd) return root === subRoot
 
     const isMismatch = root.val !== subRoot.val
