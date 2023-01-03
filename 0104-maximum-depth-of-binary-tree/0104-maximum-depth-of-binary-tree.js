148b9f90bc52bea4a2c0d9bd987555dfb1dbcecc
JB Kam <92565826+jb3k@users.noreply.github.com>
2023-01-03 11:37:29 -0800

    Time: 90 ms (60.14%), Space: 44.7 MB (75.96%) - LeetHub

--- 0104-maximum-depth-of-binary-tree/0104-maximum-depth-of-binary-tree.js
+++ 0104-maximum-depth-of-binary-tree/0104-maximum-depth-of-binary-tree.js
@@ -11,6 +11,7 @@
  * @return {number}
  */
 var maxDepth = function(root) {
-    if (!root) return 0;
-    return Math.max(maxDepth(root.left), maxDepth(root.right)) + 1;
+    if (!root) return null
+    return 1 + Math.max(maxDepth(root.left), maxDepth(root.right))
+
 };
