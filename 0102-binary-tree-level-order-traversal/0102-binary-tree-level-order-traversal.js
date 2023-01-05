47e1129f0541bebc70d93830d4594bf45237a0f4
JB Kam <92565826+jb3k@users.noreply.github.com>
2023-01-05 15:32:42 -0800

    Time: 104 ms (40.93%), Space: 44.1 MB (61.14%) - LeetHub

--- 0102-binary-tree-level-order-traversal/0102-binary-tree-level-order-traversal.js
+++ 0102-binary-tree-level-order-traversal/0102-binary-tree-level-order-traversal.js
@@ -11,26 +11,34 @@
  * @return {number[][]}
  */
 var levelOrder = function(root) {
-    // let arr = [], let level = 1
-    // conditional for empty trees
-    // traverse thru tree
-        // conditional 1) if root.left and root.right exist level +1, add those values as a to arr as an arr, then move to next
-        // conditional 2) if only one side exists, level++, add the value to arr as arr, then move to next
-    //return arr
     
-    let q = [root], ans = []
-    while (q[0]) {
-        let qlen = q.length, row = []
-        for (let i = 0; i < qlen; i++) {
-            let curr = q.shift()
-            row.push(curr.val)
-            if (curr.left) q.push(curr.left)
-            if (curr.right) q.push(curr.right)
-        }
-        ans.push(row)            
-    }
-    return ans
+    // If root is null return an empty array
+    if(!root) return []
     
+    const queue = [root] // initialize the queue with root
+    const res = [] // declare output array
+    
+    while(queue.length !== 0){
+       const queueLength = queue.length // Get the length prior to dequeueing
+       const currLevel = [] // Declare this level
+       // loop through to exhaust all options and only to include nodes at currLevel
+       for(let i = 0; i < queueLength; i++){
+           // Get next node
+           const current = queue.shift()
+           
+           if(current.left){
+               queue.push(current.left)
+           }
+           if(current.right){
+               queue.push(current.right)
+           }
+           // After we add left and right for current, we add to currLevel
+           currLevel.push(current.val)
+       }
+       // Level has been finished. Push into output array
+       res.push(currLevel)
+   }
+    return res
     
     
     
