3de57fb22fe01939b7ee8559e2ecbeb9cb0baa47
JB Kam <92565826+jb3k@users.noreply.github.com>
2023-01-30 21:43:55 -0800

    Time: 63 ms (81.69%), Space: 42.8 MB (26.89%) - LeetHub

---
+++ 0062-unique-paths/0062-unique-paths.js
@@ -0,0 +1,19 @@
+/**
+ * @param {number} m
+ * @param {number} n
+ * @return {number}
+ */
+var uniquePaths = function(m, n) {
+    let dp = new Array(m).fill(0).map(() => new Array(n));
+    for (let row = m-1; row >= 0; row--) {
+        for (let col = n-1; col >= 0; col--) {
+            if (row === m-1 || col === n-1) {
+                dp[row][col] = 1;
+            } else {
+                dp[row][col] = dp[row][col+1] + dp[row+1][col];
+            }
+        }
+    }
+    return dp[0][0];
+
+};
