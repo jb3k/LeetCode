3a0b0d10560911b0defea104d8e94a3169c109d3
JB Kam <92565826+jb3k@users.noreply.github.com>
2023-01-30 20:07:35 -0800

    Time: 74 ms (77.23%), Space: 42.2 MB (89.48%) - LeetHub

--- 0647-palindromic-substrings/0647-palindromic-substrings.js
+++ 0647-palindromic-substrings/0647-palindromic-substrings.js
@@ -3,33 +3,52 @@
  * @return {number}
  */
 var countSubstrings = function(s) {
-    
-    let count = 0
-    let [l,r] = [0,1]
-    
-    while(l <= s.length){
-        while(r <= s.length){
-            if(helper(s.substring(l,r))){
-            count++
-            }
-          r++
-        }
-        l++
-      	r = l + 1
-        
+   
+    let count = 0;
+  for (let i = 0; i < s.length; i++) {
+    expand(i, i) // odd length
+    expand(i, i + 1) // even length
+  }
+  return count
+
+  function expand(l, r) {
+    while (l >= 0 && r < s.length && s[l] === s[r]) {
+      count++
+      l--
+      r++
     }
-    return count
+  }
+    
+    
+    
+    
+    
+//     let count = 0
+//     let [l,r] = [0,1]
+    
+//     while(l <= s.length){
+//         while(r <= s.length){
+//             if(helper(s.substring(l,r))){
+//             count++
+//             }
+//           r++
+//         }
+//         l++
+//       	r = l + 1
+        
+//     }
+//     return count
     
 
 };
 
 
-var helper = function(word){
-    if (word.length === 1) return true
-    let str = ''
-    for(let i = word.length - 1; i >= 0; i--){
-        str += word[i]
-    }
+// var helper = function(word){
+//     if (word.length === 1) return true
+//     let str = ''
+//     for(let i = word.length - 1; i >= 0; i--){
+//         str += word[i]
+//     }
     
-    return str === word
-}
+//     return str === word
+// }
