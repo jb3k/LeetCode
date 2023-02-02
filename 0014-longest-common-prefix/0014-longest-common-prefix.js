d9c0f601fa6dce69333a06df0960b658867c12c0
JB Kam <92565826+jb3k@users.noreply.github.com>
2023-02-02 11:46:55 -0800

    Time: 76 ms (50.12%), Space: 44.4 MB (11.14%) - LeetHub

--- 0014-longest-common-prefix/0014-longest-common-prefix.js
+++ 0014-longest-common-prefix/0014-longest-common-prefix.js
@@ -2,13 +2,15 @@
  * @param {string[]} strs
  * @return {string}
  */
-var longestCommonPrefix = function(strs) {
-    if(!strs.length) return '';
+var longestCommonPrefix = function(strs) { 
+    if(!strs.length) return ''
     
-    for(let i = 0; i < strs[0].length; i++) {
-        for(let s of strs) {
-            if(s[i] !== strs[0][i]) return s.slice(0, i);
+    for(let i = 0; i < strs[0].length; i++){
+        for(let s of strs){
+            if(s[i] !== strs[0][i]) return s.slice(0,i)   
         }
     }
-    return strs[0];
+    
+    return strs[0]
+    
 };
