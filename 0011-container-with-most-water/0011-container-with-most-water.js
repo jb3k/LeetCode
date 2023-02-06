71fd5fc92288333690763bd4a1e1957bbeae8e82
JB Kam <92565826+jb3k@users.noreply.github.com>
2023-02-06 10:11:23 -0800

    Time: 87 ms (51.93%), Space: 49.1 MB (75.05%) - LeetHub

--- 0011-container-with-most-water/0011-container-with-most-water.js
+++ 0011-container-with-most-water/0011-container-with-most-water.js
@@ -3,22 +3,48 @@
  * @return {number}
  */
 var maxArea = function(nums) {
+    //max = -infinity
+    //double for loop starting first ind, 2nd at i+1
+    //find the width, has to be the shorter one, find smaller between two points
+    //find min height between two points (for area)
+    //multiply w x h, if greater than max, make new max
     
-  if(nums.length <= 2) return Math.min(...nums)
-  
-  let area = 0
-  let [l, r] = [0, nums.length -1]
-  
-  while (l <= r) {
-  	let dif = r - l
-    let min = Math.min(nums[l], nums[r])
-    area = Math.max(area, dif * min)
+//     let max = -Infinity;
+//     for(let i = 0; i < nums.length; i++){
+//         for(let j = i + 1; j < nums.length; j++){
+//             let width = j - i
+//             let height = Math.min(nums[j], nums[i]);
+//             max = Math.max(max, width * height);
+            
+//         }
+//     }
     
-    if(nums[l] <= nums[r]) l++
-    else r--
-  
-  }
-  
-  return area
-
+//     return max
+    
+    
+    //instantiate var
+    //while loop pointer1<pointer2
+    //find width and height
+    //find area
+    //pointers move either increment or decrement. if the height of first pointer < height pointer2, use higher height
+    //return max
+    
+    
+    let pointer1 = 0
+    let pointer2 = nums.length - 1;
+    let max = -Infinity;
+    
+    while(pointer1 < pointer2){
+        let width = pointer2 - pointer1;
+        let height = Math.min(nums[pointer2], nums[pointer1]);
+        let area = width * height
+        max = Math.max(max, area)
+        
+        if(nums[pointer1] > nums[pointer2]) pointer2--
+        else pointer1++
+        
+    }
+    return max
+    
+    
 };
