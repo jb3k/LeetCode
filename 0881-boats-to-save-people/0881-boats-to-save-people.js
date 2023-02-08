94bf9e732d94c8d556f265921194d5d64f9c3b4c
JB Kam <92565826+jb3k@users.noreply.github.com>
2023-02-08 12:00:12 -0800

    Time: 156 ms (94.77%), Space: 49.3 MB (82.77%) - LeetHub

---
+++ 0881-boats-to-save-people/0881-boats-to-save-people.js
@@ -0,0 +1,34 @@
+/**
+ * @param {number[]} people
+ * @param {number} limit
+ * @return {number}
+ */
+var numRescueBoats = function(people, limit) {
+    
+    //sort people from lowest to highest
+    //pointers at first and last
+    //counter for boats
+    //add everything together
+    
+    //1,2,2,3
+    const sorted = people.sort((a,b) => a-b)
+    let left = 0
+    let right = people.length -1
+    let counter = 0
+    
+    while(left <= right){
+        let weight = sorted[left] + sorted[right];
+        if(weight <= limit){
+            left++
+            right--  
+        }else{
+            right--  
+        }
+        counter++   
+        
+    }
+    
+    return counter
+    
+    
+};
