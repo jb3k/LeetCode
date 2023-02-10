577c2c1abe49cb309bd40feb5ddba6daedf6a943
JB Kam <92565826+jb3k@users.noreply.github.com>
2023-02-10 10:54:55 -0800

    Time: 85 ms (28.96%), Space: 43.6 MB (96.07%) - LeetHub

--- 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.js
+++ 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.js
@@ -11,28 +11,38 @@
  * @return {ListNode}
  */
 var mergeTwoLists = function(list1, list2) {
-      
-    let sentinel = tail = new ListNode();
-
-    while (list1 && list2) {/* Time O(N + M) */
-        const isL2Greater = list1.val <= list2.val;
-        const isL2Less = list2.val < list1.val;
-
-        if (isL2Greater) {
-            tail.next = list1;
-            list1 = list1.next;
+     
+    //if no list1 && list2 return empty list
+    //create new list
+    // compare values of l1 and l2
+    // if l1 val <= l2.val then new list1 = list.next
+        // then move list1 pointer to next value
+    //else you do the opposite
+    //if you have no more values in 1 list then next value is just = to list with values
+    
+    // if(!list1 && !list2) return []
+    
+    let newList = tail = new ListNode();
+    
+    while(list1 && list2){
+        
+        if(list1.val <= list2.val){
+            tail.next = list1
+            list1 = list1.next
+        }else{
+            tail.next = list2
+            list2 = list2.next
         }
-
-        if (isL2Less) {
-            tail.next = list2;
-            list2 = list2.next;
-        }
-
-        tail = tail.next;
+        
+        tail = tail.next
+        
     }
-
-    tail.next = list1 || list2;
-
-    return sentinel.next;
+    
+    // if(!list1) newList.next = list2
+    // if(!list2) newList.next = list1
+     tail.next = list1 || list2;
+    
+    return newList.next
+    
     
 };
