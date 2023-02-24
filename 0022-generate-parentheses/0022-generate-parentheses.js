4a3dfde33a551a008e9d0efb65a350bc2781fb3b
JB Kam <92565826+jb3k@users.noreply.github.com>
2023-02-24 13:24:55 -0800

    Time: 213 ms (5.01%), Space: 51.4 MB (5.08%) - LeetHub

--- 0022-generate-parentheses/0022-generate-parentheses.js
+++ 0022-generate-parentheses/0022-generate-parentheses.js
@@ -3,32 +3,26 @@
  * @return {string[]}
  */
 var generateParenthesis = function(n) {
-    // let stack = []
-    // if n = 1 return ['()']
-    // determine what a valid par are
-        // if n = 3... then im going to have to have 3 open and 3 close
-        // note* always has to be an open before a close
-    
+    //backtracking
     let stack = []
     let res = []
-    
     let backtrack = (open, close) => {
+        console.log(stack)
+        //basecase
         if(open === n && close === n){
             return res.push(stack.join(''))
         }
-        
         if(open < n){
             stack.push('(')
             backtrack(open + 1, close)
             stack.pop()
         }
-        
+        //close can never be greater than open because if not its invalid
         if(close < open){
             stack.push(')')
             backtrack(open, close + 1)
             stack.pop()
-        }
-        
+        }    
     }
     
     backtrack(0,0)
