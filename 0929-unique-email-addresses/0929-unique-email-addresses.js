055692d5e04f1b0baf1ef72f32df0676f204de49
JB Kam <92565826+jb3k@users.noreply.github.com>
2023-02-27 11:54:23 -0800

    Time: 72 ms (92.26%), Space: 46.7 MB (47.26%) - LeetHub

---
+++ 0929-unique-email-addresses/0929-unique-email-addresses.js
@@ -0,0 +1,38 @@
+/**
+ * @param {string[]} emails
+ * @return {number}
+ */
+    
+    //local name, domain name...
+        // '.' local name... basically doesnt do anything
+        // '+' local name... ignores everything after
+    
+    // let set = new Set()
+    // loop thru emails
+        // split each email by @
+        // let new email = ''
+        // loop thru the local name
+            // if local has '.' then not going to add the new email
+            // if local has '+' split it from there and only take 
+        // add the domain name to new email
+        // email = new email
+        // set.push(email)
+    // return set.size
+        
+    
+var numUniqueEmails = function(emails) {
+    const set = new Set()
+    
+    for (let email of emails) {
+        const address = normalizeEmail(email)
+        set.add(address)
+    }
+    
+    return set.size
+};
+
+function normalizeEmail(email) {
+    let [local, domain] = email.split("@");
+    local = local.replace(/(\.)|(\+.*)/g, "")
+    return local + "@" + domain
+}
