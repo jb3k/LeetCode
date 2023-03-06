edab9f8aefed17bfb8e2456724530b3676b5962a
JB Kam <92565826+jb3k@users.noreply.github.com>
2023-03-05 22:09:56 -0800

    Time: 59 ms (95.53%), Space: 44.2 MB (44.50%) - LeetHub

--- 0496-next-greater-element-i/0496-next-greater-element-i.js
+++ 0496-next-greater-element-i/0496-next-greater-element-i.js
@@ -4,32 +4,37 @@
  * @return {number[]}
  */
 var nextGreaterElement = function(nums1, nums2) {
-    
-    // let res = []
-    // loop thru nums1
-        // find the index of the character in nums 2 from nums1
-        // while idx2 < nums2.length
-            // if a num is greater than the cur num then push into res
-            // increment idx
-        // 
-    
-    let res = []
-    for(let i = 0; i < nums1.length; i++){
-        let num = nums1[i];
-        let index = nums2.indexOf(num)
-        while(index < nums2.length){
-            if(nums2[index] > num){
-                res.push(nums2[index])
-                break
-            }
-            index++
-        }
-            if(res.length - 1  !== i){
-                res.push(-1)
-            }
+
+//     let res = []
+//     for(let i = 0; i < nums1.length; i++){
+//         let num = nums1[i];
+//         let index = nums2.indexOf(num)
+//         while(index < nums2.length){
+//             if(nums2[index] > num){
+//                 res.push(nums2[index])
+//                 break
+//             }
+//             index++
+//         }
+//         if(res.length - 1  !== i){
+//             res.push(-1)
+//         }
             
+//     }
+//     return res
+    
+  const map = {};
+  const stack = [];
+  nums2.forEach(n => {
+    while (stack.length > 0 && stack[stack.length - 1] < n) {
+      map[stack.pop()] = n;
     }
-    return res
+    stack.push(n);
+  });
+  return nums1.map(n => map[n] || -1);
+    
+    
+    
     
     
 };
