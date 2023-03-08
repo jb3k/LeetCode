1e0d19df19d22757caca6465fb6817ce615e5eb2
JB Kam <92565826+jb3k@users.noreply.github.com>
2023-03-08 10:22:04 -0800

    Time: 102 ms (40.97%), Space: 53.5 MB (5.21%) - LeetHub

--- 0143-reorder-list/0143-reorder-list.js
+++ 0143-reorder-list/0143-reorder-list.js
@@ -10,47 +10,67 @@
  * @return {void} Do not return anything, modify head in-place instead.
  */
 var reorderList = function(head) {
-    const mid = getMid(head);           /* Time O(N) */
-    const reveredFromMid = reverse(mid);/* Time O(N) */
-
-    reorder(head, reveredFromMid);      /* Time O(N) */
+  
+    //finding mid
+        //two pointers, fast slow
+        //while (fast.next.next)
+            //move slow pointer 1
+            //move fast pointer 2
+    
+    //reverse 2nd half
+        //let secondHalf = slow.next
+        //prev, cur, next
+        //while curr
+            //next = cur.next
+            //cur.next = prev
+            //prev = cur
+            //cur = next
+    
+    //slow.next = null
+    //reorder
+        //h1 = head
+        //h2 = cur
+        //while(h1)
+            //let next = h1.next
+            //h1.next = h2
+            //h1 = h2
+            //h2 = next
+    let slow = head
+    let fast = head
+    while(fast && fast.next){
+        slow = slow.next
+        fast = fast.next.next
+    }
+    
+    let secondHalf = slow.next
+    let prev = null
+    let cur = secondHalf
+    let next = null
+    while(cur){
+        next = cur.next
+        cur.next = prev
+        prev = cur
+        cur = next
+        
+    }
+    
+    slow.next = null
+    let h1 = head
+    let h2 = prev
+    console.log(h1, h2)
+    while(h2){
+        let next = h1.next
+        h1.next = h2
+        h1 = h2
+        h2 = next
+        
+    }
+    
+    
+    
+    
+    
+    
+    
 };
 
-const getMid = (head) => {
-    let [ slow, fast ] = [ head, head ];
-
-    while (fast && fast.next) {         /* Time O(N) */
-        slow = slow.next;
-        fast = fast.next.next;
-    }
-
-    return slow;
-}
-
-const reverse = (head) => {
-    let [ prev, curr, next ] = [ null, head, null ];
-
-    while (curr) {                      /* Time O(N) */
-        next = curr.next;
-        curr.next = prev;
-
-        prev = curr;
-        curr = next;
-    }
-
-    return prev;
-}
-
-const reorder = (l1, l2) => {
-    let [ first, next, second ] = [ l1, null, l2 ];
-
-    while (second.next) {              /* Time O(N) */
-        next = first.next;
-        first.next = second;
-        first = next;
-
-        next = second.next;
-        second.next = first;
-        second = next;
-    }
-}
