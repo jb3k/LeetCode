bff71365e82aeb16da6faeee79e5a55ce0edd683
JB Kam <92565826+jb3k@users.noreply.github.com>
2023-03-22 10:23:50 -0700

    Time: 67 ms (85.54%), Space: 43.7 MB (86.93%) - LeetHub

--- 0735-asteroid-collision/0735-asteroid-collision.js
+++ 0735-asteroid-collision/0735-asteroid-collision.js
@@ -4,41 +4,35 @@
  */
 var asteroidCollision = function(asteroids) {
     
-    // makee a stack
-    // loop thru asteroids
-    // push first asteroid into stack
-    // only push into stack if positive
-    // if (asteroids[i] > 0) stack.push(a[i])
-    // else let pop = stack.pop()
-        //if Math.abs(pop) > Math.abs(asteroids[i])
-        //stack.push(pop)
-        // else if(Math.abs(pop)< Math.abs(a[i]))
-        // i-- 
-        // else continue
-    // return stack
     
+    const stack = []
     
-    let stack = []
-    for(let i = 0; i < asteroids.length; i++){
-        let a = asteroids[i];
-        
-        if(a < 0 && stack.length === 0 || stack[stack.length -1] < 0){
-            stack.push(a)
-        } else if(a > 0){
-            stack.push(a)
-        } else{
-            let pop = stack.pop();
-            if(Math.abs(pop) > Math.abs(a)){
-                stack.push(pop)
-            }else if(Math.abs(pop)< Math.abs(a)){
+    for (let i = 0; i < asteroids.length; i++) {
+        const last = stack[stack.length - 1]
+        const curr = asteroids[i]
+        if (last > 0 && curr < 0) {
+			// colliding
+            if (last + curr === 0) {
+                stack.pop()
+            } else if (Math.abs(last) < Math.abs(curr)) {
+                stack.pop()
                 i--
-            }else {
-                continue
-            }  
-            
-        }
+            }
+			
+        } else {
+			stack.push(curr)
+		}
     }
-
+    
     return stack
+    
+    
+    
+    
+    
+    
+    
+    
+    
     
 };
