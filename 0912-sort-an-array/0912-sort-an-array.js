17cccb6b0023f803154a59fa8702dc58e3ed8e2d
JB Kam <92565826+jb3k@users.noreply.github.com>
2023-03-23 13:35:47 -0700

    Time: 395 ms (44.68%), Space: 69.4 MB (5.09%) - LeetHub

--- 0912-sort-an-array/0912-sort-an-array.js
+++ 0912-sort-an-array/0912-sort-an-array.js
@@ -4,50 +4,25 @@
  */
 var sortArray = function(nums) {
     
-    let merge = (arr, l, m, r) => {
-        let leftArr = arr.slice(l, m + 1)
-        let rightArr = arr.slice(m + 1, r+ 1)
-        
-        let cur = l
-        let left = 0
-        let right = 0
-        
-        while(left < leftArr.length && right < rightArr.length){
-            if(leftArr[left] <= rightArr[right]){
-                arr[cur] = leftArr[left]
-                left++
-            } else {
-                arr[cur] = rightArr[right]
-                right++
-            }
-            cur++
-        }
-        while(left < leftArr.length){
-            arr[cur] = leftArr[left]
-            left++
-            cur++
-        }
-        while(right < rightArr.length){
-            arr[cur] = rightArr[right]
-            right++
-            cur++
-        }
-    }
-    
-    
-    
-    let mergeSort = (arr, l, r) => {
-        // basecase
-        if(l === r) return arr
-        
-        let m = Math.floor((l + r) / 2)
-        mergeSort(arr, l, m)
-        mergeSort(arr, m + 1, r)
-        merge(arr, l, m, r)
-        return arr
-        
-    }
-    
-    return mergeSort(nums, 0, nums.length -1)
-    
+    if (nums.length <= 1) return nums;
+
+    const mid = Math.floor(nums.length / 2);
+    const leftNums = nums.slice(0, mid);
+    const rightNums = nums.slice(mid);
+
+    return merge(sortArray(leftNums), sortArray(rightNums));
 };
+
+function merge(leftNums, rightNums) {
+    const sortedResult = [];
+    let i = 0;
+    let j = 0;
+    while (i < leftNums.length && j < rightNums.length) {
+        if (leftNums[i] <= rightNums[j]) {
+            sortedResult.push(leftNums[i++]);
+        } else {
+            sortedResult.push(rightNums[j++]);
+        }
+    }
+    return [...sortedResult, ...leftNums.slice(i), ...rightNums.slice(j)];
+}
