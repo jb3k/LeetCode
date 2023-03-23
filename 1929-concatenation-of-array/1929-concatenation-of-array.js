bb49fae5f664302edbdf5fd55c25886f11031c5b
JB Kam <92565826+jb3k@users.noreply.github.com>
2023-03-23 11:04:14 -0700

    Time: 67 ms (95.43%), Space: 45.8 MB (34.25%) - LeetHub

--- 1929-concatenation-of-array/1929-concatenation-of-array.js
+++ 1929-concatenation-of-array/1929-concatenation-of-array.js
@@ -5,13 +5,8 @@
 var getConcatenation = function(nums) {
     
     let res = []
-    for(let i = 0; i < nums.length; i++){
-        let num = nums[i];
-        res.push(num)
-    }
-    for(let i = 0; i < nums.length; i++){
-        let num = nums[i];
-        res.push(num)
+    for(let i = 0; i < 2; i++){
+        res.push(...nums)
     }
     return res
     
