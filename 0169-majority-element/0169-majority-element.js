770c662280d00341484b862f352e5c6a20c18784
JB Kam <92565826+jb3k@users.noreply.github.com>
2023-04-05 12:55:17 -0700

    Time: 62 ms (85.24%), Space: 42.8 MB (97.29%) - LeetHub

--- 0169-majority-element/0169-majority-element.js
+++ 0169-majority-element/0169-majority-element.js
@@ -4,9 +4,7 @@
  */
 var majorityElement = function(nums) {
 
-
     let obj = {};
-    
     for(let i = 0; i < nums.length; i++){
         obj[nums[i]] = obj[nums[i]] + 1 || 1;
         if(obj[nums[i]] > nums.length / 2)  return nums[i];
