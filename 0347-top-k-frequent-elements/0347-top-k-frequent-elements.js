05af05fd1d56f538aa536a93c9afcf21b67f21a9
JB Kam <92565826+jb3k@users.noreply.github.com>
2023-04-18 17:59:11 -0700

    Time: 190 ms (5.05%), Space: 60.4 MB (5.10%) - LeetHub

--- 0347-top-k-frequent-elements/0347-top-k-frequent-elements.js
+++ 0347-top-k-frequent-elements/0347-top-k-frequent-elements.js
@@ -5,21 +5,34 @@
  */
 var topKFrequent = function(nums, k) {
 
-    let frequency = {}
-    for( let i = 0; i < nums.length; i++){
-        if(frequency.hasOwnProperty(nums[i])) frequency[nums[i]] += 1;
-        else frequency[nums[i]] = 1;
-    }
-    let result = Object.keys(frequency).map((key) => [Number(key), frequency[key]]);
-    let sortedResult = result.sort((a,b) => {
-        return b[1]-a[1]
-    })
-    let output = []
-    for ( let i = 0; i < k; i++){
-        output.push(sortedResult[i][0])
-    }
-    return output;
+    if(nums.length <= 1) return nums
     
+    let obj = {} // {1:3, 2:2, 3:1}
+    let freq = {} // { '1': [], '2': [], '3': [], '4': [], '5': [], '6': [] }
+    nums.forEach((num, i) => {
+        freq[i + 1] = []
+        if(obj[num]) obj[num] += 1
+        else obj[num] = 1    
+    })
+    
+    // loop thru obj and add them to the freq based on their frequency
+    for(let key in obj){
+        let value = obj[key]
+        freq[value].push(key)
+    }
+    
+    console.log(freq)
+    
+    let res = []
+    //get keys of the freq obj so we can loop thru with reg for loop
+    let keys = Object.keys(freq)
+    for(let i = keys.length - 1; i >= 0; i--){
+        let key = keys[i]
+        for(let num of freq[key]){
+            res.push(num)
+            if(res.length === k) return res
+        }
+    }
     
     
 };
