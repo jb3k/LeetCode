f2316e386339611af59b325a08cf18df2ca4419e
JB Kam <92565826+jb3k@users.noreply.github.com>
2023-04-18 17:06:18 -0700

    Time: 81 ms (48.21%), Space: 47.3 MB (23.08%) - LeetHub

--- 0347-top-k-frequent-elements/0347-top-k-frequent-elements.js
+++ 0347-top-k-frequent-elements/0347-top-k-frequent-elements.js
@@ -4,34 +4,25 @@
  * @return {number[]}
  */
 var topKFrequent = function(nums, k) {
-    
-    let bucket = []
-  let target = []
-  let hash = {}
-  for(let i = 0; i < nums.length; i++){
-  	let num = nums[i];
-    if(!hash[num]){
-    	hash[num] = 1
-    } else {
-    	hash[num] += 1
-    }
-  }
 
-	for(let [key, value] of Object.entries(hash)){
-		if(!bucket[value]){
-		bucket[value] = new Set().add(key)
-		} else {
-		bucket[value] = bucket[value].add(key)
-		}
-	}  
-  
- 	 for(let i = bucket.length - 1; i >= 0; i--){
-  		let num = bucket[i]
-        console.log(num)
-    	if(bucket[i]) target.push(...num)
-      	if(target.length === k) break
-  	}
-	
-  	return target
+    const freqMap = new Map();
+    const bucket = [];
+    const result = [];
+    
+    for(let num of nums) {
+        freqMap.set(num, (freqMap.get(num) || 0) + 1);
+    }
+    
+    for(let [num, freq] of freqMap) {
+        bucket[freq] = (bucket[freq] || new Set()).add(num);
+    }
+    
+    for(let i = bucket.length-1; i >= 0; i--) {
+        if(bucket[i]) result.push(...bucket[i]);
+        if(result.length === k) break;
+    }
+    return result;
+    
+    
     
 };
