12dc65c5ca398d684ddacb198900b146cd3f4b6f
JB Kam <92565826+jb3k@users.noreply.github.com>
2023-04-20 14:45:27 -0700

    Time: 90 ms (96.91%), Space: 58.3 MB (32.02%) - LeetHub

--- 0128-longest-consecutive-sequence/0128-longest-consecutive-sequence.js
+++ 0128-longest-consecutive-sequence/0128-longest-consecutive-sequence.js
@@ -3,38 +3,40 @@
  * @return {number}
  */
 var longestConsecutive = function(nums) {
-//     let set = new Set(nums)
-//     let count = 1
-//     let max = 0
+
     
-//     if(nums.length === 0) return 0
+    // set to get rid of reoccuring nums
+    // how do i know where im starting? i want to start from either the beginning or the end
+
     
-//     for(let num of set){
-//         if(set.has(num + 1) && !set.has(num - 1)){
-//             while(set.has(num + 1)){
-//                 num++
-//                 count++
-//             }
-//         }
-//         max = Math.max(count, max)
-//         count = 1
-//     }
-//     return max
+        // Approach: Starting from the largest
+        // loop thru the set, if there is a number that is 1 higher than the current number just continue... if theres nothing higher thats 1 integer above, then i can start my count.
+        // have a variable that keeps track of the count... then going to start from that number which should be the highest number, then see if the set has that number -1, if it does add to the count if not, find the next number to move to
     
-        const set = new Set(nums)
+    // increment from starting point to see if theres a number +1 or -1 depending on where i start          from
     
-    let longest = 0
+    
+    let numbers = new Set(nums);
+    let max = 0
+    let count = 1
+    let pointer
     
     for(let i = 0; i < nums.length; i++){
-        if (!set.has(nums[i] - 1)){
-            let length = 0
-            while(set.has(nums[i] + length)){
-                length++
-                longest = Math.max(length, longest)
+        let num = nums[i];
+        if(!numbers.has(num+1)){
+            pointer = num
+            while(numbers.has(pointer - 1)){
+                count++
+                pointer--
             }
+            max = Math.max(max, count)
+            count = 1
         }
+        
     }
-    return longest
     
     
+    return max
+    
+        
 };
