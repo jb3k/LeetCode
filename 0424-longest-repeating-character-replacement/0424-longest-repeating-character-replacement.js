57f76839fa9a5305c234226762d12175ba20c655
JB Kam <92565826+jb3k@users.noreply.github.com>
2023-04-21 15:50:52 -0700

    Time: 79 ms (66.67%), Space: 42.3 MB (77.51%) - LeetHub

--- 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.js
+++ 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.js
@@ -4,20 +4,7 @@
  * @return {number}
  */
 var characterReplacement = function(s, k) {
-    //two pointer, l,r = 0, 0
-    // counter obj, length var
-    
-    // while r < s.length 
-    // obj[s[r]] === null ? obj[s[r]] = 1 : obj[s[r]] + 1
-    // if the counter of a letter is greater than length var
-        //set that to new length var
-    // if the window of r - l + 1 (+1 includes the last character) - the length var is greater than k, it means that it's not a valid window
-        //decrement the count of the left, and move the left pointer up (to make it a valid window)
-    
-    // r++
 
-    //return the distance between r and l
-    
   let left = 0;
   let right = 0;
   let maxCharCount = 0;
@@ -38,4 +25,6 @@ var characterReplacement = function(s, k) {
   }
 
   return right - left;
+    
+    
 };
