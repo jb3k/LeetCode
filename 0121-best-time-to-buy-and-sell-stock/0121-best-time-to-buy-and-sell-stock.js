0f6070c9ab040e51e660be8e23d2cf74aada2019
JB Kam <92565826+jb3k@users.noreply.github.com>
2023-04-21 12:00:17 -0700

    Time: 86 ms (49.37%), Space: 51.6 MB (70.03%) - LeetHub

--- 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.js
+++ 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.js
@@ -3,21 +3,30 @@
  * @return {number}
  */
 var maxProfit = function(list) {
-  let pointer = 0
-  let left = 0
-  let right = 1
-  
-  while( right < list.length){
-  	if(list[left] < list[right]){
-    	let profit = list[right] - list[left]
-        pointer = Math.max(pointer, profit)
-    }
-    else{
-    	left = right
-    }
-    right++  
-  }
 
-  return pointer
+    //two pointers, but i want to keep track of the difference
+        // i want the left pointer to be smallest and the right pointer to the be           the biggest
+    // start from 0,1
+    // loop thru while r < list.length
+    //if l >= r, then increment both
+    // else find and store profit then  r++
+    
+    
+    let profit = 0;
+    let [l,r] = [0,1];
+    
+    while(r < list.length){
+        if(list[l] >= list[r]){
+            l = r
+            r++
+            
+        } else {
+            let sum = list[r] - list[l]
+            profit = Math.max(profit, sum)
+            r++
+        }
+    }
+    
+    return profit
 
 };
