bd8fe3cf7dd3262e6893c01ed3eebdb6df346ec9
JB Kam <92565826+jb3k@users.noreply.github.com>
2023-04-21 11:31:29 -0700

    Time: 83 ms (32.14%), Space: 50 MB (24.68%) - LeetHub

--- 0011-container-with-most-water/0011-container-with-most-water.js
+++ 0011-container-with-most-water/0011-container-with-most-water.js
@@ -3,48 +3,27 @@
  * @return {number}
  */
 var maxArea = function(nums) {
-    //max = -infinity
-    //double for loop starting first ind, 2nd at i+1
-    //find the width, has to be the shorter one, find smaller between two points
-    //find min height between two points (for area)
-    //multiply w x h, if greater than max, make new max
     
-//     let max = -Infinity;
-//     for(let i = 0; i < nums.length; i++){
-//         for(let j = i + 1; j < nums.length; j++){
-//             let width = j - i
-//             let height = Math.min(nums[j], nums[i]);
-//             max = Math.max(max, width * height);
-            
-//         }
-//     }
-    
-//     return max
+    // 2 pointers starting from beginning and end
+    // var to store the max
+    // loop thru, while l < r
+    // calc the area
+    // move the l / r pointer based on whatever is smaller
     
     
-    //instantiate var
-    //while loop pointer1<pointer2
-    //find width and height
-    //find area
-    //pointers move either increment or decrement. if the height of first pointer < height pointer2, use higher height
-    //return max
+    let max = 0
+    let [l,r] = [0, nums.length -1]
     
-    
-    let pointer1 = 0
-    let pointer2 = nums.length - 1;
-    let max = -Infinity;
-    
-    while(pointer1 < pointer2){
-        let width = pointer2 - pointer1;
-        let height = Math.min(nums[pointer2], nums[pointer1]);
-        let area = width * height
+    while(l < r){
+        let height = Math.min(nums[l], nums[r]);
+        let width = r - l;
+        let area = height * width
         max = Math.max(max, area)
-        
-        if(nums[pointer1] > nums[pointer2]) pointer2--
-        else pointer1++
+        if(nums[r] >= nums[l]) l++
+        else r--
         
     }
-    return max
     
+    return max
     
 };
