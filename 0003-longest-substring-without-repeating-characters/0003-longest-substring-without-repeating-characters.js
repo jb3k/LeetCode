9ef9b02c3737118221f7cf59be9317d3db7bbe6e
JB Kam <92565826+jb3k@users.noreply.github.com>
2023-04-21 15:15:41 -0700

    Time: 75 ms (91.13%), Space: 46.9 MB (69.17%) - LeetHub

--- 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.js
+++ 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.js
@@ -6,10 +6,10 @@ var lengthOfLongestSubstring = function(s) {
     
     let set = new Set();
     let left = 0;
-    let maxSize = 0;
+    let maxSize = 1;
 
     if (s.length === 0) return 0;
-    if (s.length === 1) return 1;
+    // if (s.length === 1) return 1;
 
     for (let i = 0; i < s.length; i++) {
 
