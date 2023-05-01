9d8823a9a02f503ece8e68ed5919e7fa7bcec95b
JB Kam <92565826+jb3k@users.noreply.github.com>
2023-05-01 13:29:25 -0700

    Time: 50 ms (97.63%), Space: 41.8 MB (94.02%) - LeetHub

--- 0020-valid-parentheses/0020-valid-parentheses.js
+++ 0020-valid-parentheses/0020-valid-parentheses.js
@@ -4,14 +4,6 @@
  */
 var isValid = function(s) {
     
-    //want to have as a stack
-    // make an object that stores the 3 char
-    // loop thru s
-        // push the first char into stack
-        // conditional that checks if the value of the first 
-        // 
-    
-    // return the stack length === 0
     let stack = []
     let hash = {
         ')':'(',
