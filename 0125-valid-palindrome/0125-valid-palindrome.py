9385a5006ba5daff4f9fdd929978a754c026a3a4
JB Kam <92565826+jb3k@users.noreply.github.com>
2023-05-13 22:59:19 -0700

    Time: 48 ms (69.39%), Space: 17.7 MB (15.37%) - LeetHub

---
+++ 0125-valid-palindrome/0125-valid-palindrome.py
@@ -0,0 +1,14 @@
+class Solution:
+    def isPalindrome(self, s: str) -> bool:
+        
+        if len(s) == 1:
+            return True
+        
+        alph = 'abcdefghijklmnopqrstuvwxyz0123456789'
+        lower = s.lower()
+        arr = [letters for letters in lower if letters in alph]
+        newStr = ''.join(arr)
+        rev = newStr[::-1]
+        print(newStr, rev)
+        
+        return newStr == rev
